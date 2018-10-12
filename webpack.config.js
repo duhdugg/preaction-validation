d5d47b4950322359e51e4f2b1348400e29368078
Doug Elkin <doug@drelyn.net>
2018-10-11 23:04:05 -0400

    preparing for distribution

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+let path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    base: './lib/base.js',
+    date: './lib/date.js',
+    email: './lib/email.js',
+    empty: './lib/empty.js',
+    isbn10: './lib/isbn10.js',
+    isb13: './lib/isbn13.js',
+    notEmpty: './lib/notEmpty.js',
+    number: './lib/number.js',
+    phone: './lib/phone.js',
+    price: './lib/price.js',
+    salesforceid: './lib/salesforceid.js',
+    url: './lib/url.js'
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|__tests__)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
