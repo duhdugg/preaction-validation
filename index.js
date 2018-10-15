f3fd9a5ad74c75b10cd0344969dbbf753452f6c4
Doug Elkin <doug@drelyn.net>
2018-10-15 14:32:01 -0400

    using es6 exports

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-import baseValidator from './dist/base.min.js'
-import dateValidator from './dist/date.min.js'
-import emailValidator from './dist/email.min.js'
-import emptyValidator from './dist/empty.min.js'
-import isbn10Validator from './dist/isbn10.min.js'
-import isbn13Validator from './dist/isbn13.min.js'
-import notEmptyValidator from './dist/notEmpty.min.js'
-import numberValidator from './dist/number.min.js'
-import phoneValidator from './dist/phone.min.js'
-import priceValidator from './dist/price.min.js'
-import salesforceidValidator from './dist/salesforceid.min.js'
-import urlValidator from './dist/url.min.js'
-
-export { baseValidator }
-export { dateValidator }
-export { emailValidator }
-export { emptyValidator }
-export { isbn10Validator }
-export { isbn13Validator }
-export { numberValidator }
-export { phoneValidator }
-export { priceValidator }
-export { salesforceidValidator }
-export { urlValidator }
-
-export default {
-  baseValidator,
-  dateValidator,
-  emailValidator,
-  emptyValidator,
-  isbn10Validator,
-  isbn13Validator,
-  notEmptyValidator,
-  numberValidator,
-  phoneValidator,
-  priceValidator,
-  salesforceidValidator,
-  urlValidator
-}
+export { baseValidator } from './dist/base.min.js'
+export { dateValidator } from './dist/date.min.js'
+export { emailValidator } from './dist/email.min.js'
+export { emptyValidator } from './dist/empty.min.js'
+export { isbn10Validator } from './dist/isbn10.min.js'
+export { isbn13Validator } from './dist/isbn13.min.js'
+export { notEmptyValidator } from './dist/notEmpty.min.js'
+export { numberValidator } from './dist/number.min.js'
+export { phoneValidator } from './dist/phone.min.js'
+export { priceValidator } from './dist/price.min.js'
+export { salesforceidValidator } from './dist/salesforceid.min.js'
+export { urlValidator } from './dist/url.min.js'
