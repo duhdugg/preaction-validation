ec207e1f86146ee74d9f8fe88d6b01f60fd46d82
Doug Elkin <doug@drelyn.net>
2018-10-15 14:04:06 -0400

    fixing index

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ import emptyValidator from './dist/empty.min.js'
 import isbn10Validator from './dist/isbn10.min.js'
 import isbn13Validator from './dist/isbn13.min.js'
 import notEmptyValidator from './dist/notEmpty.min.js'
+import numberValidator from './dist/number.min.js'
+import phoneValidator from './dist/phone.min.js'
+import priceValidator from './dist/price.min.js'
+import salesforceidValidator from './dist/salesforceid.min.js'
+import urlValidator from './dist/url.min.js'
+
+export { baseValidator }
+export { dateValidator }
+export { emailValidator }
+export { emptyValidator }
+export { isbn10Validator }
+export { isbn13Validator }
+export { numberValidator }
+export { phoneValidator }
+export { priceValidator }
+export { salesforceidValidator }
+export { urlValidator }
 
 export default {
   baseValidator,
@@ -13,5 +30,10 @@ export default {
   emptyValidator,
   isbn10Validator,
   isbn13Validator,
-  notEmptyValidator
+  notEmptyValidator,
+  numberValidator,
+  phoneValidator,
+  priceValidator,
+  salesforceidValidator,
+  urlValidator
 }
