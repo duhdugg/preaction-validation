955ae82ba58a478e12fe7b1663ce15788479973d
Doug Elkin <doug@drelyn.net>
2018-10-15 13:49:27 -0400

    changing index (hopefully this fixes it)

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-export { default as baseValidator } from './dist/base.min.js'
-export { default as dateValidator } from './dist/date.min.js'
-export { default as emailValidator } from './dist/email.min.js'
-export { default as emptyValidator } from './dist/empty.min.js'
-export { default as isbn13Validator } from './dist/isbn13.min.js'
-export { default as isbn10Validator } from './dist/isbn10.min.js'
-export { default as notEmptyValidator } from './dist/notEmpty.min.js'
-export { default as numberValidator } from './dist/number.min.js'
-export { default as phoneValidator } from './dist/phone.min.js'
-export { default as priceValidator } from './dist/price.min.js'
-export { default as salesforceidValidator } from './dist/salesforceid.min.js'
-export { default as urlValidator } from './dist/url.min.js'
+import baseValidator from './dist/base.min.js'
+import dateValidator from './dist/date.min.js'
+import emailValidator from './dist/email.min.js'
+import emptyValidator from './dist/empty.min.js'
+import isbn10Validator from './dist/isbn10.min.js'
+import isbn13Validator from './dist/isbn13.min.js'
+import notEmptyValidator from './dist/notEmpty.min.js'
+
+export {
+  baseValidator,
+  dateValidator,
+  emailValidator,
+  emptyValidator,
+  isbn10Validator,
+  isbn13Validator,
+  notEmptyValidator
+}
