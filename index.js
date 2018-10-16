28e47baed583bd903c2427e811b6c4167ead1d4a
Doug Elkin <doug@drelyn.net>
2018-10-15 21:55:37 -0400

    cleaning up tests and index

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const baseValidator = require('./dist/base.min.js').default
-const dateValidator = require('./dist/date.min.js').default
-const emailValidator = require('./dist/email.min.js').default
-const emptyValidator = require('./dist/empty.min.js').default
-const isbn10Validator = require('./dist/isbn10.min.js').default
-const isbn13Validator = require('./dist/isbn13.min.js').default
-const notEmptyValidator = require('./dist/notEmpty.min.js').default
-const numberValidator = require('./dist/number.min.js').default
-const phoneValidator = require('./dist/phone.min.js').default
-const priceValidator = require('./dist/price.min.js').default
-const salesforceIdValidator = require('./dist/salesforceid.min.js').default
-const urlValidator = require('./dist/url.min.js').default
+const baseValidator = require('./dist/base.min.js')
+const dateValidator = require('./dist/date.min.js')
+const emailValidator = require('./dist/email.min.js')
+const emptyValidator = require('./dist/empty.min.js')
+const isbn10Validator = require('./dist/isbn10.min.js')
+const isbn13Validator = require('./dist/isbn13.min.js')
+const notEmptyValidator = require('./dist/notEmpty.min.js')
+const numberValidator = require('./dist/number.min.js')
+const phoneValidator = require('./dist/phone.min.js')
+const priceValidator = require('./dist/price.min.js')
+const salesforceIdValidator = require('./dist/salesforceid.min.js')
+const urlValidator = require('./dist/url.min.js')
 
 module.exports = {
   baseValidator,
@@ -23,19 +23,6 @@ module.exports = {
   phoneValidator,
   priceValidator,
   salesforceIdValidator,
-  urlValidator,
-  default: {
-    baseValidator,
-    dateValidator,
-    emailValidator,
-    emptyValidator,
-    isbn10Validator,
-    isbn13Validator,
-    notEmptyValidator,
-    numberValidator,
-    phoneValidator,
-    priceValidator,
-    salesforceIdValidator,
-    urlValidator
-  }
+  urlValidator
 }
+module.exports.default = module.exports
